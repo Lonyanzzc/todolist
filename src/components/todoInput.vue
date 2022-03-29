8f3b8c42dfb735a2b63a54503f457296b2f19e86
Lonyanzzc <1278726024@qq.com>
2022-03-30 00:26:40 +0800

    feat：添加之前忘记写的修改功能

--- src/components/todoInput.vue
+++ src/components/todoInput.vue
@@ -3,12 +3,17 @@
 	  <van-row justify="center">
         <van-col span="21">
           <van-cell-group>
-            <van-field v-model="input" label="todo" :colon="true" :center="true" placeholder="请随意输入" :autofocus="true" ref="field">
+            <van-field v-model="input" label="todo" 
+              @keydown.enter="enter"
+              :colon="true" 
+              :center="true" 
+              placeholder="请随意输入" 
+              :autofocus="true" ref="field">
             	<template #button>
             		<van-button size="small" type="primary" @click="submit">提交</van-button>
             	</template>
-                </van-field>
-            </van-cell-group>
+            </van-field>
+          </van-cell-group>
         </van-col>
       </van-row>
     </div>
@@ -27,11 +32,18 @@ export default defineComponent({
       input.value =''
       field.value.focus()
     }
+
+    const enter = () => {
+      context.emit("add-list", input.value)
+      input.value =''
+      field.value.focus()
+    }
     
     return {
       input,
       field,
-      submit
+      submit,
+      enter
     }
   }
 })
--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -5,8 +5,12 @@
 				<van-checkbox-group>
 					<van-cell-group inset>
 						<van-cell v-for="(item, index) in lists" clickable :key="item.value"
-							:title="item.value"
+							@dblclick="selectInput = index"
 							@click="toggle(index)">
+							<template #title>
+								<span v-show="selectInput !== index">{{item.value}}</span>
+								<input v-if="selectInput === index" class="input" :value="item.value" @blur="blur(item, $event)">
+							</template>
 							<template #icon>
 								<van-checkbox
 								:name="item.value"
@@ -37,6 +41,7 @@ export default {
 		lists: Array
 	},
 	setup(props, context) {	
+		let selectInput = ref()
 		const toggle = (index) => {
 			context.emit("checked", index)
 		};
@@ -45,22 +50,40 @@ export default {
 			context.emit("removeItem", index)
 		};
 
+		const blur = (item, e) => {
+			selectInput.value = -1
+			item.value = e.target.value
+			console.log(item);
+		}
+
 		return {
 			toggle,
-			removeItem
+			removeItem,
+			blur,
+			selectInput
 		};
 	},
 }
 </script>
 
 <style>
-
 .todolist div[class="van-cell__title"]{
+	position: relative;
 	white-space: nowrap;
 	text-overflow: ellipsis;
 	overflow: hidden;
 	padding: 0 40px;
 	box-sizing: border-box;
 	width: 100%;
+}
+
+.input{
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	z-index: 100;
+	top: 0;
+	left: 0;
+	font-size: 20px;
 }
 </style>
